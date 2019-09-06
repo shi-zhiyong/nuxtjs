1c5924de278323183a68fa5213a8eb063ef687ea
szy <503305354@qq.com>
2019-09-06 09:15:35 +0800

    first commit

---
+++ pages/home/_id.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home-id-page">
+    _id.vue
+    {{ info }}
+  </div>
+</template>
+
+<script>
+  export default {
+    asyncData(context) {
+      console.log('asyncData', context);
+      return {info: "hello world"}
+    },
+    fetch({ store, params }) {
+      console.log('fetch', store);
+      console.log('fetch', params);
+    },
+    beforeCreate() {
+    },
+    created() {
+    },
+    beforeMount() {
+      console.log('beforeMount',window);
+    },
+    mounted() {
+      console.log('mounted',window);
+    }
+  }
+</script>
+
+<style
+  scoped>
+
+</style>
---
+++ pages/user/_id/user-detail.vue
@@ -0,0 +1,13 @@
+<template>
+<div>user-detail</div>
+</template>
+
+<script>
+    export default {
+        name: "user-detail"
+    }
+</script>
+
+<style scoped>
+
+</style>
